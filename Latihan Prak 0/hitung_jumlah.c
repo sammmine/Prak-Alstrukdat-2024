f48c547926c561ae42b0b90ba8f297ccdd9e3f7b
Samantha Laqueenna Ginting <167011131+sammmine@users.noreply.github.com>
2024-09-23 19:40:13 +0700

    feat: update lat prak 0

--- Latihan Prak 0/hitung_jumlah.c
+++ Latihan Prak 0/hitung_jumlah.c
@@ -8,21 +8,21 @@ int main() {
     int sum = 0;
     scanf("%d %d", &n, &k);
 
-    if (n>=1 && n<=1000 && k>=1 && k<=1000) {
-        while (i <= n) {
-        scanf("%d", &a_i);
+    while (i <= n) {
+    scanf("%d", &a_i);
         
-        if (a_i>=1 && a_i<=10000) {
+    if (a_i>=1 && a_i<=10000) {
         
-            if (a_i%k != 0) {
-                sum = sum + a_i;
-            }
-    
-            i++;
-            }
+        if (a_i%k != 0) {
+            sum = sum + a_i;
         }
-
-        printf("%d\n", sum);
-    }
     
+        i++;
+        }
+    }
+
+    printf("%d\n", sum);
+
+    
+    return 0;
 }
