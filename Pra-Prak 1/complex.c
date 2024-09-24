d660940af1e0b3d0bf1682ff1090e6f037d83bb5
Samantha Laqueenna Ginting <167011131+sammmine@users.noreply.github.com>
2024-09-24 11:24:13 +0700

    feat: update pra-prak 1

--- Pra-Prak 1/complex.c
+++ Pra-Prak 1/complex.c
@@ -19,7 +19,7 @@ void BacaCOMPLEX(COMPLEX *C) {
     scanf("%.2f", &Im);
 
     if (IsCOMPLEXValid(Re, Im)) {
-        CreateComplex(C, Re, Im);
+        CreateComplex(&C, Re, Im);
     } else {
         printf("Input tidak valid.\n");
     }
@@ -30,3 +30,73 @@ void TulisCOMPLEX(COMPLEX C) {
     // dot operator
 }
 
+COMPLEX AddCOMPLEX(COMPLEX C1, COMPLEX C2) {
+    COMPLEX Csum;
+
+    (Csum).Re = (C1).Re + (C2).Re;
+    (Csum).Im = (C1).Im + (C2).Im;
+
+    return Csum;
+}
+
+COMPLEX SubtractCOMPLEX(COMPLEX C1, COMPLEX C2) {
+    COMPLEX Csub;
+
+    (Csub).Re = (C1).Re - (C2).Re;
+    (Csub).Im = (C1).Im - (C2).Im;
+
+    return Csub;
+}
+
+COMPLEX MultiplyCOMPLEX(COMPLEX C1, COMPLEX C2) {
+    COMPLEX Cmul;
+
+    (Cmul).Re = (C1).Re * (C2).Re - (C1).Im * (C2).Im;
+    (Cmul).Im = (C1).Re * (C2).Im + (C1).Im * (C2).Re;
+
+    return Cmul;
+}
+
+COMPLEX DivideCOMPLEX(COMPLEX C1, COMPLEX C2){
+    COMPLEX Cdiv;
+    float denom;
+
+    denom = pow((C2).Re, 2) + pow((C2).Im, 2);
+
+    if (denom == 0) {
+        (Cdiv).Re = 0;
+        (Cdiv).Im = 0;
+
+    } else {
+        ConjugateCOMPLEX(C2);
+        Cdiv = MultiplyCOMPLEX(C1, C2);
+        (Cdiv).Re = (Cdiv).Re / denom;
+        (Cdiv).Im = (Cdiv).Im / denom;
+ 
+    }
+
+    return Cdiv;
+}
+
+float AbsCOMPLEX(COMPLEX C) {
+    float modC;
+
+    modC = sqrt(pow((C).Re, 2) + pow((C).Im, 2));
+
+    return modC;
+}
+
+COMPLEX ConjugateCOMPLEX(COMPLEX C) {
+    // konjugasi itu hanya menegatifkan komponen imajinernya
+    (C).Im = 0 - (C).Im;
+
+    return C;
+}
+
+boolean CEQ(COMPLEX C1, COMPLEX C2) {
+    return ((C1).Re == (C2).Re && (C1).Im == (C2).Im);
+}
+
+boolean CNEQ(COMPLEX C1, COMPLEX C2) {
+    return ((C1).Re != (C2).Re || (C1).Im != (C2).Im || ((C1).Re != (C2).Re && (C1).Im != (C2).Im));
+}
